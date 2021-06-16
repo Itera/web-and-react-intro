b953c7ecb7bb0b8a2bd570b0aca50cce4a1a70de
Son Thanh Vo <sonvo95@outlook.com>
2021-06-16 18:30:48 +0200

    fix: Use localhost instead of 0.0.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
-const pkg = require("./package.json");
-const path = require("path");
-const glob = require("glob");
-const yargs = require("yargs");
-const colors = require("colors");
-const through = require("through2");
-const qunit = require("node-qunit-puppeteer");
+const pkg = require('./package.json');
+const path = require('path');
+const glob = require('glob');
+const yargs = require('yargs');
+const colors = require('colors');
+const through = require('through2');
+const qunit = require('node-qunit-puppeteer');
 
-const { rollup } = require("rollup");
-const { terser } = require("rollup-plugin-terser");
-const babel = require("@rollup/plugin-babel").default;
-const commonjs = require("@rollup/plugin-commonjs");
-const resolve = require("@rollup/plugin-node-resolve").default;
-const sass = require("sass");
+const { rollup } = require('rollup');
+const { terser } = require('rollup-plugin-terser');
+const babel = require('@rollup/plugin-babel').default;
+const commonjs = require('@rollup/plugin-commonjs');
+const resolve = require('@rollup/plugin-node-resolve').default;
+const sass = require('sass');
 
-const gulp = require("gulp");
-const tap = require("gulp-tap");
-const zip = require("gulp-zip");
-const header = require("gulp-header");
-const eslint = require("gulp-eslint");
-const minify = require("gulp-clean-css");
-const connect = require("gulp-connect");
-const autoprefixer = require("gulp-autoprefixer");
+const gulp = require('gulp');
+const tap = require('gulp-tap');
+const zip = require('gulp-zip');
+const header = require('gulp-header');
+const eslint = require('gulp-eslint');
+const minify = require('gulp-clean-css');
+const connect = require('gulp-connect');
+const autoprefixer = require('gulp-autoprefixer');
 
-const root = yargs.argv.root || ".";
+const root = yargs.argv.root || '.';
 const port = yargs.argv.port || 8000;
 
 const banner = `/*!
@@ -37,17 +37,17 @@ const banner = `/*!
 process.setMaxListeners(20);
 
 const babelConfig = {
-  babelHelpers: "bundled",
-  ignore: ["node_modules"],
+  babelHelpers: 'bundled',
+  ignore: ['node_modules'],
   compact: false,
-  extensions: [".js", ".html"],
-  plugins: ["transform-html-import-to-string"],
+  extensions: ['.js', '.html'],
+  plugins: ['transform-html-import-to-string'],
   presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
         corejs: 3,
-        useBuiltIns: "usage",
+        useBuiltIns: 'usage',
         modules: false,
       },
     ],
@@ -61,16 +61,16 @@ const babelConfig = {
 const babelConfigESM = JSON.parse(JSON.stringify(babelConfig));
 babelConfigESM.presets[0][1].targets = {
   browsers: [
-    "last 2 Chrome versions",
-    "not Chrome < 60",
-    "last 2 Safari versions",
-    "not Safari < 10.1",
-    "last 2 iOS versions",
-    "not iOS < 10.3",
-    "last 2 Firefox versions",
-    "not Firefox < 60",
-    "last 2 Edge versions",
-    "not Edge < 16",
+    'last 2 Chrome versions',
+    'not Chrome < 60',
+    'last 2 Safari versions',
+    'not Safari < 10.1',
+    'last 2 iOS versions',
+    'not iOS < 10.3',
+    'last 2 Firefox versions',
+    'not Firefox < 60',
+    'last 2 Edge versions',
+    'not Edge < 16',
   ],
 };
 
@@ -78,17 +78,17 @@ let cache = {};
 
 // Creates a bundle with broad browser support, exposed
 // as UMD
-gulp.task("js-es5", () => {
+gulp.task('js-es5', () => {
   return rollup({
     cache: cache.umd,
-    input: "js/index.js",
+    input: 'js/index.js',
     plugins: [resolve(), commonjs(), babel(babelConfig), terser()],
   }).then((bundle) => {
     cache.umd = bundle.cache;
     return bundle.write({
-      name: "Reveal",
-      file: "./dist/reveal.js",
-      format: "umd",
+      name: 'Reveal',
+      file: './dist/reveal.js',
+      format: 'umd',
       banner: banner,
       sourcemap: true,
     });
@@ -96,42 +96,42 @@ gulp.task("js-es5", () => {
 });
 
 // Creates an ES module bundle
-gulp.task("js-es6", () => {
+gulp.task('js-es6', () => {
   return rollup({
     cache: cache.esm,
-    input: "js/index.js",
+    input: 'js/index.js',
     plugins: [resolve(), commonjs(), babel(babelConfigESM), terser()],
   }).then((bundle) => {
     cache.esm = bundle.cache;
     return bundle.write({
-      file: "./dist/reveal.esm.js",
-      format: "es",
+      file: './dist/reveal.esm.js',
+      format: 'es',
       banner: banner,
       sourcemap: true,
     });
   });
 });
-gulp.task("js", gulp.parallel("js-es5", "js-es6"));
+gulp.task('js', gulp.parallel('js-es5', 'js-es6'));
 
 // Creates a UMD and ES module bundle for each of our
 // built-in plugins
-gulp.task("plugins", () => {
+gulp.task('plugins', () => {
   return Promise.all(
     [
       {
-        name: "RevealHighlight",
-        input: "./plugin/highlight/plugin.js",
-        output: "./plugin/highlight/highlight",
+        name: 'RevealHighlight',
+        input: './plugin/highlight/plugin.js',
+        output: './plugin/highlight/highlight',
       },
       {
-        name: "RevealNotes",
-        input: "./plugin/notes/plugin.js",
-        output: "./plugin/notes/notes",
+        name: 'RevealNotes',
+        input: './plugin/notes/plugin.js',
+        output: './plugin/notes/notes',
       },
       {
-        name: "RevealNotes",
-        input: "./plugin/cue-code-highlight/plugin.js",
-        output: "./plugin/cue-code-highlight/cue-code-highlight",
+        name: 'RevealNotes',
+        input: './plugin/cue-code-highlight/plugin.js',
+        output: './plugin/cue-code-highlight/cue-code-highlight',
       },
     ].map((plugin) => {
       return rollup({
@@ -149,15 +149,15 @@ gulp.task("plugins", () => {
       }).then((bundle) => {
         cache[plugin.input] = bundle.cache;
         bundle.write({
-          file: plugin.output + ".esm.js",
+          file: plugin.output + '.esm.js',
           name: plugin.name,
-          format: "es",
+          format: 'es',
         });
 
         bundle.write({
-          file: plugin.output + ".js",
+          file: plugin.output + '.js',
           name: plugin.name,
-          format: "umd",
+          format: 'umd',
         });
       });
     })
@@ -172,14 +172,14 @@ function compileSass() {
     sass.render(
       {
         data: transformedFile.contents.toString(),
-        includePaths: ["css/", "css/theme/template"],
+        includePaths: ['css/', 'css/theme/template'],
       },
       (err, result) => {
         if (err) {
           console.log(vinylFile.path);
           console.log(err.formatted);
         } else {
-          transformedFile.extname = ".css";
+          transformedFile.extname = '.css';
           transformedFile.contents = result.css;
           callback(null, transformedFile);
         }
@@ -188,46 +188,46 @@ function compileSass() {
   });
 }
 
-gulp.task("css-themes", () =>
+gulp.task('css-themes', () =>
   gulp
-    .src(["./css/theme/source/*.{sass,scss}"])
+    .src(['./css/theme/source/*.{sass,scss}'])
     .pipe(compileSass())
-    .pipe(gulp.dest("./dist/theme"))
+    .pipe(gulp.dest('./dist/theme'))
 );
 
-gulp.task("css-core", () =>
+gulp.task('css-core', () =>
   gulp
-    .src(["css/reveal.scss"])
-    .pipe(compileSass())
-    .pipe(autoprefixer())
-    .pipe(minify({ compatibility: "ie9" }))
-    .pipe(header(banner))
-    .pipe(gulp.dest("./dist"))
-);
-
-gulp.task("css-custom", () =>
-  gulp
-    .src(["css/custom.scss"])
+    .src(['css/reveal.scss'])
     .pipe(compileSass())
     .pipe(autoprefixer())
-    .pipe(minify({ compatibility: "ie9" }))
+    .pipe(minify({ compatibility: 'ie9' }))
     .pipe(header(banner))
-    .pipe(gulp.dest("./dist"))
+    .pipe(gulp.dest('./dist'))
 );
 
-gulp.task("css", gulp.parallel("css-themes", "css-core", "css-custom"));
+gulp.task('css-custom', () =>
+  gulp
+    .src(['css/custom.scss'])
+    .pipe(compileSass())
+    .pipe(autoprefixer())
+    .pipe(minify({ compatibility: 'ie9' }))
+    .pipe(header(banner))
+    .pipe(gulp.dest('./dist'))
+);
 
-gulp.task("qunit", () => {
+gulp.task('css', gulp.parallel('css-themes', 'css-core', 'css-custom'));
+
+gulp.task('qunit', () => {
   let serverConfig = {
     root,
     port: 8009,
-    host: "localhost",
-    name: "test-server",
+    host: 'localhost',
+    name: 'test-server',
   };
 
   let server = connect.server(serverConfig);
 
-  let testFiles = glob.sync("test/*.html");
+  let testFiles = glob.sync('test/*.html');
 
   let totalTests = 0;
   let failingTests = 0;
@@ -240,12 +240,12 @@ gulp.task("qunit", () => {
             targetUrl: `http://${serverConfig.host}:${serverConfig.port}/${filename}`,
             timeout: 20000,
             redirectConsole: false,
-            puppeteerArgs: ["--allow-file-access-from-files"],
+            puppeteerArgs: ['--allow-file-access-from-files'],
           })
           .then((result) => {
             if (result.stats.failed > 0) {
               console.log(
-                `${"!"} ${filename} [${result.stats.passed}/${
+                `${'!'} ${filename} [${result.stats.passed}/${
                   result.stats.total
                 }] in ${result.stats.runtime}ms`.red
               );
@@ -253,7 +253,7 @@ gulp.task("qunit", () => {
               qunit.printFailedTests(result, console);
             } else {
               console.log(
-                `${"✔"} ${filename} [${result.stats.passed}/${
+                `${'✔'} ${filename} [${result.stats.passed}/${
                   result.stats.total
                 }] in ${result.stats.runtime}ms`.green
               );
@@ -278,7 +278,7 @@ gulp.task("qunit", () => {
         if (failingTests > 0) {
           reject(new Error(`${failingTests}/${totalTests} tests failed`.red));
         } else {
-          console.log(`${"✔"} Passed ${totalTests} tests`.green.bold);
+          console.log(`${'✔'} Passed ${totalTests} tests`.green.bold);
           resolve();
         }
       })
@@ -291,61 +291,61 @@ gulp.task("qunit", () => {
   });
 });
 
-gulp.task("eslint", () =>
-  gulp.src(["./js/**", "gulpfile.js"]).pipe(eslint()).pipe(eslint.format())
+gulp.task('eslint', () =>
+  gulp.src(['./js/**', 'gulpfile.js']).pipe(eslint()).pipe(eslint.format())
 );
 
-gulp.task("test", gulp.series("eslint", "qunit"));
+gulp.task('test', gulp.series('eslint', 'qunit'));
 
 gulp.task(
-  "default",
-  gulp.series(gulp.parallel("js", "css", "plugins"), "test")
+  'default',
+  gulp.series(gulp.parallel('js', 'css', 'plugins'), 'test')
 );
 
-gulp.task("build", gulp.parallel("js", "css", "plugins"));
+gulp.task('build', gulp.parallel('js', 'css', 'plugins'));
 
 gulp.task(
-  "package",
-  gulp.series("default", () =>
+  'package',
+  gulp.series('default', () =>
     gulp
       .src([
-        "./index.html",
-        "./dist/**",
-        "./lib/**",
-        "./images/**",
-        "./plugin/**",
-        "./**.md",
+        './index.html',
+        './dist/**',
+        './lib/**',
+        './images/**',
+        './plugin/**',
+        './**.md',
       ])
-      .pipe(zip("reveal-js-presentation.zip"))
-      .pipe(gulp.dest("./"))
+      .pipe(zip('reveal-js-presentation.zip'))
+      .pipe(gulp.dest('./'))
   )
 );
 
-gulp.task("reload", () => gulp.src(["*.html", "*.md"]).pipe(connect.reload()));
+gulp.task('reload', () => gulp.src(['*.html', '*.md']).pipe(connect.reload()));
 
-gulp.task("serve", () => {
+gulp.task('serve', () => {
   connect.server({
     root: root,
     port: port,
-    host: "0.0.0.0",
+    host: 'localhost',
     livereload: true,
   });
 
-  gulp.watch(["*.html", "*.md"], gulp.series("reload"));
+  gulp.watch(['*.html', '*.md'], gulp.series('reload'));
 
-  gulp.watch(["js/**"], gulp.series("js", "reload", "test"));
+  gulp.watch(['js/**'], gulp.series('js', 'reload', 'test'));
 
-  gulp.watch(["plugin/**/plugin.js"], gulp.series("plugins", "reload"));
+  gulp.watch(['plugin/**/plugin.js'], gulp.series('plugins', 'reload'));
 
   gulp.watch(
-    ["css/theme/source/*.{sass,scss}", "css/theme/template/*.{sass,scss}"],
-    gulp.series("css-themes", "reload")
+    ['css/theme/source/*.{sass,scss}', 'css/theme/template/*.{sass,scss}'],
+    gulp.series('css-themes', 'reload')
   );
 
   gulp.watch(
-    ["css/*.scss", "css/print/*.{sass,scss,css}"],
-    gulp.series("css-core", "css-custom", "reload")
+    ['css/*.scss', 'css/print/*.{sass,scss,css}'],
+    gulp.series('css-core', 'css-custom', 'reload')
   );
 
-  gulp.watch(["test/*.html"], gulp.series("test"));
+  gulp.watch(['test/*.html'], gulp.series('test'));
 });
