116d952f65a9c6ef18ecc64d7230171a96492d31
Tin Anh Nguyen <anhtin95@gmail.com>
2022-08-31 15:37:35 +0200

    Move away from reveal.js template

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import "./overrides.scss";
+import CueCodeHighlight from "./plugins/cue-code-highlight";
+import External from "./plugins/external";
+import confetti from "canvas-confetti";
+import Reveal from "reveal.js";
+import "reveal.js/dist/reset.css";
+import "reveal.js/dist/reveal.css";
+import "reveal.js/dist/theme/black.css";
+import Highlight from "reveal.js/plugin/highlight/highlight";
+import "reveal.js/plugin/highlight/monokai.css";
+
+const deck = new Reveal({
+  hash: true,
+  plugins: [External, Highlight, CueCodeHighlight],
+});
+deck.initialize();
+
+setupConfetti();
+
+function setupConfetti() {
+  const svg = document.getElementById("confetti");
+  svg.setAttribute("width", window.innerWidth);
+  svg.setAttribute("height", window.innerHeight);
+
+  const throwConfetti = confetti.create(svg, {
+    resize: true,
+    useWorker: true,
+    disableForReducedMotion: true,
+  });
+  deck.addEventListener("slidechanged", () => {
+    if (deck.getCurrentSlide().classList.contains("confetti")) {
+      setTimeout(() => {
+        throwConfetti({
+          origin: { x: 0.5, y: 1.5 },
+          particleCount: 200,
+          spread: 90,
+          startVelocity: 200,
+          ticks: 300,
+        });
+      }, 300);
+    }
+  });
+}
