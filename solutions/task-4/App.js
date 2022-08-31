bdc5f0caddc132151409e9f0fbdebfe326d0dfff
Tin Anh Nguyen <anhtin95@gmail.com>
2022-08-31 14:16:23 +0200

    fix: typos from when tasks where simplified

--- solutions/task-4/App.js
+++ solutions/task-4/App.js
@@ -1,11 +1,11 @@
-import ColleagueTable from "./components/ColleagueList";
+import ColleagueList from "./components/ColleagueList";
 import "./App.css";
 
 function App() {
   return (
     <main id="task-4">
       <h1>My Colleagues</h1>
-      <ColleagueTable />
+      <ColleagueList />
     </main>
   );
 }
